5cd63d4b40171f4460f22079b38a667cdfa379af
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-11 11:41:29 -0600

    Ingresando datos

---
+++ LeyDeGravitacion/LeyDeGravitacion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeyDeGravitacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ley de la Gravitacion Universal");
+
+            Console.WriteLine("Ingrese la masa del cuerpo 1");
+            int m1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la masa del cuerpo 2");
+            int m2 = int.Parse(Console.ReadLine());
+
+
+            Console.ReadKey();
+        }
+    }
+}
+ 
